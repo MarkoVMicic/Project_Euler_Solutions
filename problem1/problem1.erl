1b43d0421409e3f9bd2c1f462966ea97d993f3a6
MarkoVMicic <marko.v.micic@gmail.com>
2020-10-15 09:25:12 +0800

    Add problem statement and link to erlang solution

--- problem1/problem1.erl
+++ problem1/problem1.erl
@@ -1,3 +1,14 @@
+% problem link: https://projecteuler.net/problem=1
+
+
+% Problem statement: 
+
+%     If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+%     we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+%     Find the sum of all the multiples of 3 or 5 below 1000.
+
+
 -module(problem1).
 -export([naive_main/1, fast_main/1]).
 
