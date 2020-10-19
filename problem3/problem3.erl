6ede7f699ef8153783b660e3f229888f495c1a53
MarkoVMicic <marko.v.micic@gmail.com>
2020-10-19 17:35:05 +0800

    Cut down time needed to compute large prime factors.
        The key insight here is that for any number N, there is at most
        one prime factor P > sqrt(N). This means that as we divide out
        prime factors and track the quotient, we can also keep track of
        sqrt(quotient). Then if our current divisor exceeds sqrt(quotient),
        we can immediately terminate the algorithm and return the divisor
        as the largest prime factor. (currently we add it to the list of all
        prime factors and then return the list but it's straightforward to
        modify the algorithm to return only the largest prime factor).

--- problem3/problem3.erl
+++ problem3/problem3.erl
@@ -7,6 +7,12 @@
 % 
 %     What is the largest prime factor of the number 600851475143 ?
 
+% Note: Prime Factors is currently a list. Replace with a set if your only 
+% interest is in unique prime factors. 
+
+% Note: Function currently remembers all prime factors. Can change this to 
+% look for only the largest factor. 
+
 -module(problem3).
 -export([main/1]).
 
@@ -22,52 +28,74 @@ prime_factors_of_N(N) ->
     Current_Divisor = 2,
     Prime_Factors = [],
     Current_Quotient = N,
-    prime_factors_of_N(Prime_Factors, Current_Divisor, Current_Quotient).
+    Max_Factor = N,
+    prime_factors_of_N(Prime_Factors, 
+                       Current_Divisor, 
+                       Current_Quotient, 
+                       Max_Factor).
 
-% Base Case: If the Current_Quotient is 1, we are done, return the list of 
-% Prime Factors. 
-prime_factors_of_N(Prime_Factors, _Current_Divisor, 1) 
+% Naive Base Case: If the Current_Quotient is 1, we are done, return the list  
+% of Prime Factors. 
+prime_factors_of_N(Prime_Factors, 
+                   _Current_Divisor, 
+                   1, 
+                   _Max_Factor) 
 ->
     Prime_Factors;
-prime_factors_of_N([], 2, Current_Quotient)
+% KEY INSIGHT: For any number N, there is at most 1 prime factor greater than 
+%              √N. We can use this fact to prematurely stop the algorithm if 
+%              we have found the largest prime factor already!
+% Alternative Base Case: If the current divisor is greater than the square 
+% root of the current quotient, then it is the largest prime factor.
+prime_factors_of_N(Prime_Factors, 
+                   Current_Divisor, 
+                   Current_Quotient, 
+                   Max_Factor) 
+when Current_Divisor > Max_Factor 
+andalso Current_Quotient rem Current_Divisor =:= 0 ->
+    [Current_Divisor | Prime_Factors];
+% The first number we will examine is 2. This is the only place where we 
+% increment the divisor by 1 (to 3), after which we increment it by two. 
+prime_factors_of_N(Prime_Factors, 
+                   2, 
+                   Current_Quotient, 
+                   Max_Factor)
 ->
     if
-        Current_Quotient rem 2 == 0 ->
-            prime_factors_of_N([2], 2, Current_Quotient div 2);
-        Current_Quotient rem 2 /= 0 ->
-            prime_factors_of_N([], 3, Current_Quotient);
+        % Current Quotient is even
+        Current_Quotient rem 2 =:= 0 ->
+            prime_factors_of_N([2 | Prime_Factors], 
+                               2, 
+                               Current_Quotient div 2,
+                               math:sqrt(Current_Quotient div 2));
+        % Current Quotient is odd
+        Current_Quotient rem 2 =/= 0 ->
+            prime_factors_of_N(Prime_Factors, 
+                               3, 
+                               Current_Quotient,
+                               Max_Factor);
         true ->
             io:format("This shouldn't be seen! ~n")
     end;
 
 % If the current divisor divides the current quotient, it is a prime factor
-prime_factors_of_N(Prime_Factors, Current_Divisor, Current_Quotient)
+prime_factors_of_N(Prime_Factors, 
+                   Current_Divisor, 
+                   Current_Quotient, 
+                   _Max_Factor)
     when Current_Quotient rem Current_Divisor == 0 ->
         prime_factors_of_N([Current_Divisor | Prime_Factors],
                            Current_Divisor,
-                           Current_Quotient div Current_Divisor);
+                           Current_Quotient div Current_Divisor,
+                           math:sqrt(Current_Quotient div Current_Divisor));
 % If the current divisor does not divide the current quotient, increment 
-% divisor by 1. 
-prime_factors_of_N(Prime_Factors, Current_Divisor, Current_Quotient)
+% divisor by 2. 
+prime_factors_of_N(Prime_Factors, 
+                   Current_Divisor, 
+                   Current_Quotient, 
+                   Max_Factor)
     when Current_Quotient rem Current_Divisor /= 0 ->
         prime_factors_of_N(Prime_Factors,
                            Current_Divisor + 2,
-                           Current_Quotient).
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                           Current_Quotient,
+                           Max_Factor).
