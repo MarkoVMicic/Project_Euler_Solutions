f951cd7602f532ad6b4a68cbd506467ee1edad20
MarkoVMicic <marko.v.micic@gmail.com>
2020-10-19 16:00:26 +0800

    Change to compute prime factors only, instead of all prime numbers to N.
        NOTE(Marko): Still too slow. Seems like a better idea would be to just
                     divide by consecutive integers until we hit 1. This idea
                     is obvious enough that I am sad I did not see it earlier.

--- problem3/problem3.erl
+++ problem3/problem3.erl
@@ -14,50 +14,93 @@ main(N) ->
     P = largest_prime_factor(N),
     P.
 
-largest_prime_factor(N) ->
-    Primes = primes_to_N(N),
-    Primes.
+largest_prime_factor(M) ->
+    Prime_Factors = prime_factors_to_N(M), 
+    Prime_Factors.
 
-primes_to_N(N) ->
+prime_factors_to_N(N) ->
+    Prime_Factors = [2],
     Int_List = lists:seq(2, N),
-    primes_to_N(Int_List, N).
+    Current_Prime = 2,
+    prime_factors_to_N(Prime_Factors,
+                       Int_List,
+                       [],
+                       N,
+                       Current_Prime).
 
-primes_to_N(Int_List, _N) ->
-    Primes_List = [2],
-    Temp_List = [],
-    primes_to_N(Primes_List, Int_List, Temp_List).
+% Base Case: All prime factors retrieved from Int_List in reverse order. 
+prime_factors_to_N(Prime_Factors,
+                   [],
+                   [],
+                   _N,
+                   _Current_Prime)
+->
+    Prime_Factors;
 
-% Base Case: All primes retrieved from Int_List in reverse order.  
-primes_to_N(Primes_List,
-            [],
-            []) 
-->
-    Primes_List;
-% For current prime, all values in Int_List checked. Multiples of current prime already stripped from Int_List and NOT inserted into Temp List. At this stage, we want to reverse Temp_List, take the first value, put it in the list of primes, and then put the rest as the new Int_List, passing an empty list to Temp_List parameter. 
-primes_to_N(Primes,
-            [],
-            Temp_List)
-->
-    New_List = lists:reverse(Temp_List),
-    [New_Prime | Int_List] = New_List,
-    primes_to_N([New_Prime | Primes],
-                Int_List,
-                []);
-% If the current prime number evenly divides the current int, then we clearly have a composite number, so we simply remove it from the list without adding it to the temp list. 
-primes_to_N([Current_Prime | Primes_Rest],
-            [Current_Int | Int_Rest],
-            Temp_List)
+% If the current integer is evenly divided by the current prime, remove it from the Int_List
+prime_factors_to_N(Prime_Factors,
+                   [Current_Int | Rest_Int],
+                   Temp_List,
+                   N,
+                   Current_Prime)
 when Current_Int rem Current_Prime == 0 ->
-    primes_to_N([Current_Prime | Primes_Rest],
-                Int_Rest,
-                Temp_List);
-
-% If the current prime number does not evenly divide the current int, then we move the value from the int list to the temp list. 
-primes_to_N([Current_Prime | Primes_Rest],
-            [Current_Int | Int_Rest],
-            Temp_List)
+    prime_factors_to_N(Prime_Factors,
+                       Rest_Int,
+                       Temp_List,
+                       N,
+                       Current_Prime);
+% If the current integer is not evenly divided by the current prime, move it to the temp list. 
+prime_factors_to_N(Prime_Factors,
+                   [Current_Int | Rest_Int],
+                   Temp_List,
+                   N,
+                   Current_Prime)
+when Current_Int rem Current_Prime /= 0 ->
+    prime_factors_to_N(Prime_Factors,
+                       Rest_Int,
+                       [Current_Int | Temp_List],
+                       N,
+                       Current_Prime);
+% When we have exhausted the int_list, check if the first value of the new list (i.e. the list with multiples of Current Prime removed) evenly divides N. If not, it is not a prime factor of N, else it is. 
+prime_factors_to_N(Prime_Factors,
+                   [],
+                   Temp_List,
+                   N,
+                   _Current_Prime)
 ->
-    primes_to_N([Current_Prime | Primes_Rest],
-                Int_Rest,
-                [Current_Int | Temp_List]).
+    New_Prime = lists:last(Temp_List),
+    New_List = lists:droplast(Temp_List),
+    if
+        % New_Prime is a prime factor of N
+        N rem New_Prime == 0 -> 
+            prime_factors_to_N([New_Prime | Prime_Factors],
+                                lists:reverse(New_List),
+                                [],
+                                N,
+                                New_Prime);
+        % New_Prime is not a factor of N, so call this pattern of the function again. 
+        N rem New_Prime /= 0 ->
+            prime_factors_to_N(Prime_Factors,
+                               [],
+                               New_List,
+                               N,
+                               New_Prime)
+
+    end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
