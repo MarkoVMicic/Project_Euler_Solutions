f7b84b856776813da8030f154f6c83387977315b
MarkoVMicic <marko.v.micic@gmail.com>
2020-11-03 12:14:33 +0800

    Reformat problem 2 for readability.

--- problem2/problem2.erl
+++ problem2/problem2.erl
@@ -9,35 +9,54 @@
 -module(problem2).
 -export([main/1]).
 
-main(MaxValue) ->
-    Answer = sum_even_fibonacci(MaxValue),
-    Answer.
+main( Max_value ) 
+->
+    Answer = sum_even_fibonacci( Max_value ),
+    Answer
+.
 
-sum_even_fibonacci(MaxValue) ->
+sum_even_fibonacci( Max_value ) 
+->
     Index = 0,
     Acc = 0,
     F_t = 1,
     F_tminus1 = 1,
-    sum_even_fibonacci(Index, 
-                       Acc, 
-                       F_t, 
-                       F_tminus1, 
-                       MaxValue).
+    sum_even_fibonacci( Index, 
+                        Acc, 
+                        F_t, 
+                        F_tminus1, 
+                        Max_value )
+.
 
-sum_even_fibonacci(_, Acc, F_t, _, MaxValue) 
-    when F_t > MaxValue ->
-    Acc;
-sum_even_fibonacci(Index, Acc, F_t, F_tminus1, MaxValue) 
-    when Index rem 3 == 0 ->
-    sum_even_fibonacci(Index + 1, 
-                       Acc + F_t + F_tminus1, 
-                       F_t + F_tminus1, 
-                       F_t, 
-                       MaxValue);
-sum_even_fibonacci(Index, Acc, F_t, F_tminus1, MaxValue) ->
-    sum_even_fibonacci(Index + 1, 
-                       Acc, 
-                       F_t + F_tminus1, 
-                       F_t, 
-                       MaxValue).
-
+sum_even_fibonacci( _Index, 
+                    Acc, 
+                    F_t, 
+                    _F_tminus1, 
+                    Max_value ) when F_t > Max_value 
+->
+    Acc
+;
+sum_even_fibonacci( Index, 
+                    Acc, 
+                    F_t, 
+                    F_tminus1, 
+                    Max_value ) when Index rem 3 == 0 
+->
+    sum_even_fibonacci( Index + 1, 
+                        Acc + F_t + F_tminus1, 
+                        F_t + F_tminus1, 
+                        F_t, 
+                        Max_value )
+;
+sum_even_fibonacci( Index, 
+                    Acc, 
+                    F_t, 
+                    F_tminus1, 
+                    Max_value ) 
+->
+    sum_even_fibonacci( Index + 1, 
+                        Acc, 
+                        F_t + F_tminus1, 
+                        F_t, 
+                        Max_value )
+.
