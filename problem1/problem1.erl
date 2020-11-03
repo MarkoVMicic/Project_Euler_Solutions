3db465971985c84cc0a92ae8d0508d467986083a
MarkoVMicic <marko.v.micic@gmail.com>
2020-11-03 11:59:06 +0800

    Reformat problem 1 for readability.
        In general, all the reformatting of code will adhere to the
        following style guidelines:
        (1) No space between function name and bracket, then space
            between opening bracket and first argument.
        (2) arguments separated by comma and space.
        (3) Nested functions (i.e. functions called as arguments to
            functions do not use these spaces).
        (4) function names are lower-case underscored
        As an example of 1-4:
    
            some_function( arg1, arg2, another_function(Arg3), Arg4 )
    
        (5) When arg list is too long, break lines according to the
            first function e.g.
    
                function_with_long_arg_list( Arg1,
                                             Arg2 )
    
        (6) Use -> and ; or . in a similar manner to the way curly
            brackets are used in C, e.g.
    
                function( Arg1, Arg2)
                ->
                    expression1,
                    expression2
                ;
                function( Arg1, 0)
                ->
                    expression3
                .
        (7) Guard statements are not to put against the margin.
            where possible, they should be put after the closing
            bracket of the arg list e.g.
    
                function( Arg1,
                          Arg2 ) when Guard1;
                                      Guard2, Guard3
    	    ->
    		expression
                .

--- problem1/problem1.erl
+++ problem1/problem1.erl
@@ -12,29 +12,54 @@
 -module(problem1).
 -export([naive_main/1, fast_main/1]).
 
-naive_main(Max_value) ->
-    Naive = naive(Max_value),
-    Naive.
+naive_main( Max_value ) 
+->
+    Naive = naive( Max_value ),
+    Naive
+.
 
-fast_main(Max_value) ->
-    Fast = fast(Max_value),
-    Fast.
+fast_main( Max_value ) 
+->
+    Fast = fast( Max_value ),
+    Fast
+.
 
-naive(Max_value) ->
-    Naive = naive(0, Max_value-1),
-    Naive.
-naive(Acc, 0) -> Acc;
-naive(Acc, Current_value) when 
-    Current_value rem 3 == 0; Current_value rem 5 == 0 ->
-    naive(Acc+Current_value, Current_value - 1);
-naive(Acc, Current_value) ->
-    naive(Acc, Current_value - 1).
+naive( Max_value ) 
+->
+    Naive = naive( 0, Max_value-1 ),
+    Naive
+.
+naive( Acc, 0 ) -> Acc;
+naive( Acc, 
+       Current_value ) when Current_value rem 3 == 0; 
+                            Current_value rem 5 == 0 
+->
+    naive( Acc+Current_value, Current_value - 1 )
+;
+naive( Acc, Current_value ) 
+->
+    naive(Acc, Current_value - 1)
+.
 
-fast(Max_value) ->
-    Fast = sum_divisible_by(3, Max_value) + sum_divisible_by(5, Max_value) - sum_divisible_by(15, Max_value),
-    Fast.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-sum_divisible_by(Divisor, Value) ->
+fast( Max_value ) 
+->
+    Fast = sum_divisible_by( 3, Max_value ) + 
+           sum_divisible_by( 5, Max_value ) - 
+           sum_divisible_by( 15, Max_value ),
+    Fast
+.
+
+sum_divisible_by( Divisor, Value ) 
+->
     M = (Value-1) div Divisor,
     Sum = Divisor * ((M*(M+1))/2),
-    Sum.
+    Sum
+.
