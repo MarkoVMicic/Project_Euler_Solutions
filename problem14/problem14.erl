fab796be5601b98a2561307983ec2e65aaefac39
MarkoVMicic <marko.v.micic@gmail.com>
2020-11-18 11:51:34 +0800

    Improve speed of solution to problem 14 of project euler.
         Incorporating the suggestions in the previous commit, the
         program now computes the longest chain much faster. There
         appears to be a small bug in computation of the chain
         lengths (they are all short by 1) but since the challenge
         asks to find the initial value with the longest chain, it
         doesn't affect the validity of the result. Nevertheless,
         I will keep this branch open while I figure out where I've
         introduced this bug.
    
         For comparison, problem14:naive_main(5000000) takes
         around 28s to run while problem14:fast_main(5000000) takes
         around 18s.

--- problem14/problem14.erl
+++ problem14/problem14.erl
@@ -22,13 +22,21 @@
 % NOTE: Once the chain starts the terms are allowed to go above one million.
 
 -module( problem14 ).
--export( [main/1] ).
+-export( [naive_main/1, fast_main/1] ).
 
-main( N )
+naive_main( N )
 ->
     longest_chain_up_to_N( N )
 .
 
+fast_main( N )
+->
+    longest_chain_with_map( N )
+.
+
+% ---------------
+% Naive solution
+% ---------------
 longest_chain_up_to_N( N ) 
 -> 
     longest_chain_up_to_N( N, 1, 1, 0, 0, 1 )
@@ -102,3 +110,138 @@ longest_chain_up_to_N( N,
 
     end
 .
+
+% --------------
+% Fast solution
+% --------------
+longest_chain_with_map( N ) 
+-> 
+    % Use a map to store previously seen chain-lengths. See (1) in bottom 
+    % comment for details
+    % Begin searching from N div 2. See (2) in bottom comment for details. 
+    longest_chain_with_map( N div 2, 
+                            N div 2, 
+                            N div 2, 
+                            0, 
+                            0, 
+                            1, 
+                            #{} )
+.
+longest_chain_with_map( 0, 
+                       _Current_Initial, 
+                       _Current_Number, 
+                       _Chain_Length, 
+                       Longest_Length, 
+                       Longest_Initial,
+                       _Map )
+->
+    {Longest_Initial, Longest_Length+1}
+;
+longest_chain_with_map( N,
+                        Current_Initial,
+                        1,
+                        Chain_Length,
+                        Longest_Length,
+                        _Longest_Initial,
+                        Map ) when Chain_Length > Longest_Length
+->
+    longest_chain_with_map( N-1,
+                            Current_Initial + 1,
+                            Current_Initial + 1,
+                            0,
+                            Chain_Length,
+                            Current_Initial,
+                            Map#{Current_Initial => Chain_Length} )
+;
+longest_chain_with_map( N,
+                        Current_Initial,
+                        1,
+                        Chain_Length,
+                        Longest_Length,
+                        Longest_Initial,
+                        Map ) 
+->
+    longest_chain_with_map( N-1, 
+                            Current_Initial + 1,
+                            Current_Initial + 1,
+                            0,
+                            Longest_Length,
+                            Longest_Initial,
+                            Map#{Current_Initial => Chain_Length} )
+;
+% Current Number is even: if we've seen Current_Number/2 before, we can 
+% immediately compute the length (See (2))
+longest_chain_with_map( N,
+                        Current_Initial,
+                        Current_Number,
+                        Chain_Length, 
+                        Longest_Length,
+                        Longest_Initial,
+                        Map ) when Current_Number rem 2 == 0
+->
+    case Map of
+        #{(Current_Number div 2) := Stored_Length}
+        ->
+            longest_chain_with_map( N,
+                                    Current_Initial,
+                                    1,
+                                    Chain_Length + (Stored_Length + 1),
+                                    Longest_Length,
+                                    Longest_Initial,
+                                    Map )
+        ;
+        _
+        ->
+            longest_chain_with_map( N,
+                                    Current_Initial,
+                                    Current_Number div 2,
+                                    Chain_Length + 1,
+                                    Longest_Length,
+                                    Longest_Initial,
+                                    Map )
+    end
+;
+% Current Number is odd: if we've seen (3*Current_Number+1)/2 before, we can 
+% immediately compute the length (See (3))
+longest_chain_with_map( N,
+                        Current_Initial,
+                        Current_Number,
+                        Chain_Length, 
+                        Longest_Length,
+                        Longest_Initial,
+                        Map ) when Current_Number rem 2 == 1
+->
+    case Map of
+        #{((3*Current_Number + 1) div 2) := Stored_Length}
+        ->
+            longest_chain_with_map( N,
+                                    Current_Initial,
+                                    1,
+                                    Chain_Length + (Stored_Length + 2),
+                                    Longest_Length,
+                                    Longest_Initial,
+                                    Map )
+        ;
+        _
+        ->
+            longest_chain_with_map( N,
+                                    Current_Initial,
+                                    3*Current_Number +1,
+                                    Chain_Length + 1,
+                                    Longest_Length,
+                                    Longest_Initial,
+                                    Map )
+    end
+.
+
+
+% (1) We can store the values seen thus far, and if we find
+%     a previously seen value, we can simply add 1 to that,
+%     e.g. since Collatz(13) = 10, and for initial 26, the
+%     chain begins 26 -> 13, clearly
+%     Collatz(26) = 1 + Collatz(13).
+% (2) If K is even, then Collatz(K) = 1 + Collatz(K/2). Thus,
+%     for all K, Collatz(2K) > Collatz(K). Thus for input N,
+%     we need only to check the values M ≥ N/2.
+% (3) If N is odd, then 3N+1 is even, so using (2), we see
+%     that Collatz(N) = Collatz((3N + 1)/2) + 2.
