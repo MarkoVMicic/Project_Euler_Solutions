7eb56dcd1234dbf275bac812c3b688350df32429
MarkoVMicic <marko.v.micic@gmail.com>
2021-02-03 11:34:54 +0800

    Add support for even square spirals.
        Now that I analyze these two cases, I realize they are
        essentially the same. All we need is some way to compute the
        sum of the four corners of the N^2 spiral, and we can then
        iterate N upwards.
    
        The next commit will clean the code up dramatically with this
        notion in mind.

--- problem28/problem28.erl
+++ problem28/problem28.erl
@@ -24,11 +24,14 @@ main( N )
     case N rem 2 == 0 of
         true
         ->
-            even_square
+            Upper_Right = even_N_sum_upper_right_diagonal( N ),
+            Upper_Left  = even_N_sum_upper_left_diagonal( N ),
+            Lower_Left  = even_N_sum_lower_left_diagonal( N ),
+            Lower_Right = even_N_sum_lower_right_diagonal( N ),
+            Upper_Right + Upper_Left + Lower_Left + Lower_Right
         ;
         false
         ->
-            % Upper Right Diagonal
             Upper_Right = odd_N_sum_upper_right_diagonal( N ),
             Upper_Left  = odd_N_sum_upper_left_diagonal( N ),
             Lower_Left  = odd_N_sum_lower_left_diagonal( N ),
@@ -38,7 +41,127 @@ main( N )
     end
 .
 
+% --------------------
+% N is an even Number
+% --------------------
+% In an even-square (i.e. a square whose side N is an even number), the upper 
+% right diagonal consists of odd N^2 + 1 up to N .
+%
+% e.g. N = 6:
+% 
+%       24  25 (26)  --> 5^2 + 1
+%       9  (10) 27   --> 3^2 + 1
+%      (2)  11  26   --> 1^2 + 1
 
+even_N_sum_upper_right_diagonal( N )
+->
+    even_N_sum_upper_right_diagonal( N-1, 1, 0 )
+.
+even_N_sum_upper_right_diagonal( N_minus_1, 
+                                Current, 
+                                Sum ) when Current == N_minus_1
+->
+    Sum + Current*Current + 1
+;
+even_N_sum_upper_right_diagonal( N_minus_1, 
+                                Current, 
+                                Sum )
+->
+    even_N_sum_upper_right_diagonal( N_minus_1, 
+                                    Current + 2, 
+                                    Sum + Current*Current + 1 )
+.
+
+% In an even-square (i.e. a square whose side N is an even number), the upper 
+% left diagonal consists of (odd N^2 - (N-1)) up to N-1.
+%
+% e.g. N = 6:
+%
+%        (21) 22  23  --> 5^2 - 4
+%         20 (7)  8   --> 3^2 - 2
+%         19  6  (1)  --> 1^2 - 0
+
+
+even_N_sum_upper_left_diagonal( N )
+->
+    even_N_sum_upper_left_diagonal( N-1, 1, 0 )
+.
+even_N_sum_upper_left_diagonal( N_minus_1, 
+                               Current, 
+                               Sum ) when Current == N_minus_1
+->
+    Sum + (Current*Current - (Current-1))
+;
+even_N_sum_upper_left_diagonal( N_minus_1, 
+                               Current, 
+                               Sum )
+->
+    even_N_sum_upper_left_diagonal( N_minus_1, 
+                                   Current + 2, 
+                                   Sum + (Current*Current - (Current-1)) )
+.
+
+
+% In an even-square (i.e. a square whose side N is an even number), the lower
+% left diagonal consists of even N^2 up to N
+%
+% e.g. N = 6:
+% 
+%       18  5  (4)  --> 2^2
+%       17 (16) 15  --> 4^2
+%      (36) 35  34  --> 6^2
+even_N_sum_lower_left_diagonal( N )
+->
+    even_N_sum_lower_left_diagonal( N, 2, 0 )
+.
+even_N_sum_lower_left_diagonal( N,
+                               Current,
+                               Sum ) when Current == N
+->
+    Sum + Current*Current
+;
+even_N_sum_lower_left_diagonal( N,
+                               Current,
+                               Sum ) 
+->
+    even_N_sum_lower_left_diagonal( N,
+                                    Current + 2,
+                                    Sum + Current*Current)
+.
+
+
+% In an even-square (i.e. a square whose side N is an even number), the lower
+% right diagonal consists of even (N^2 - (N-1)) up to N
+%
+% e.g. N = 6:
+% 
+% 
+%       (3)  12  29  --> 2^2 - 1
+%        14 (13) 30  --> 4^2 - 3
+%        33  32 (31) --> 6^2 - 5
+even_N_sum_lower_right_diagonal( N )
+->
+    even_N_sum_lower_right_diagonal( N, 2, 0 )
+.
+even_N_sum_lower_right_diagonal( N,
+                                Current,
+                                Sum ) when Current == N
+->
+    Sum + Current*Current - (Current-1)
+;
+even_N_sum_lower_right_diagonal( N,
+                                Current,
+                                Sum )
+->
+    even_N_sum_lower_right_diagonal( N,
+                                    Current + 2,
+                                    Sum + Current*Current - (Current-1) )
+.
+
+
+% ------------------
+% N is an odd Number
+% ------------------
 % In an odd-square (i.e. a square whose side N is an odd number), the upper 
 % right diagonal (excluding the middle 1) consists of odd squares up to N.
 %
