aa25c640d86a94910364c6047b0b0cd0d3bee97c
MarkoVMicic <marko.v.micic@gmail.com>
2021-02-19 12:25:06 +0800

    Speed up erlang solution to problem35 by checking invalid digits.
        For a multi-digit prime (i.e. not single-digit) to be
        circular, it must not contain 2,4,5,6,8, or 0 as one of its
        digits. This new code checks for this, which speeds up the
        process a bit.

--- problem35/problem35.erl
+++ problem35/problem35.erl
@@ -13,7 +13,7 @@
 -module( problem35 ).
 -export( [ main/1,
            is_circular_prime/2,
-           build_list_of_primes/1 ] ).
+           build_list_of_primes/1] ).
 
 main( N )
 ->
@@ -21,11 +21,11 @@ main( N )
     find_circular_primes( Primes )
 .
 
-% TODO: Find a way to speed this up by only checking up to √N, after which all 
-%       remaining unfiltered numbers should be prime.
 % ----------------------
 % Sieve of Eratosthenes
 % ----------------------
+% TODO: Find a way to speed this up by only checking up to √N, after which all 
+%       remaining unfiltered numbers should be prime. 
 build_list_of_primes( N ) 
 ->
     build_list_of_primes( [2], lists:seq(2, N), [] )
@@ -56,15 +56,24 @@ build_list_of_primes( [Current_Prime  | Primes],
                           [Current_Number | Filtered] )
 .
 
-% TODO: Speed up by checking for even numbered digits -- instant reject
-% TODO: Speed up by checking for presence of 5 -- instant reject
+
+% -----------------------------------------------------------------------------
+% Given a list of primes, this function extracts circular primes and puts them 
+% in a list, and also counts them at the end. 
+% NOTE: Constructing the list isn't necessary to count the number of circular 
+%       primes, but since the list is small compared to the size of the Primes 
+%       list, it's not a big deal. 
+% -----------------------------------------------------------------------------
 find_circular_primes( Primes )
 ->
     find_circular_primes( Primes, Primes, [] )
 .
 find_circular_primes( Primes, [Current_Prime | Rest], Circular_Primes )
 ->
-    % TODO: This counts circular primes multiple times.
+    % TODO: This counts circular primes multiple times. Given the number of 
+    %       circular primes is small and the amount of rotations that need to 
+    %       be done is also small, optimizing this is not going to save too 
+    %       much time, but it'll still help
     case is_circular_prime( Primes, integer_to_list(Current_Prime) ) of
         true
         ->
@@ -81,20 +90,41 @@ find_circular_primes( _Primes, [], Circular_Primes )
 .
 
 
+
 is_circular_prime( _Primes, Prime_String ) when length(Prime_String) == 1
 ->
+% NOTE: All single-digit prime numbers are circular. Also since all 
+%       multi-digit prime numbers with 2 and 5 are NOT circular, we need to 
+%       make sure we return true for single-digit prime numbers FIRST. 
     true
 ;
 is_circular_prime( Primes, Prime_String )
 ->
-    Prime_Queue = queue:from_list( Prime_String ),  
-    is_circular_prime( Primes, Prime_Queue, length( Prime_String ) )
+    % Check if 2,4,5,6,8,0 are in the Prime_String.    
+    Disallowed_String_List = ["2", "4", "5", "6", "8", "0"],
+    case prime_string_has_disallowed_digit(Prime_String, 
+                                           Disallowed_String_List) of
+        true
+        ->
+            % If any of 2,4,5,6,8,0 are in the prime number, it is not a 
+            % circular prime.
+            false
+        ;
+        false
+        ->
+            % If none of 2,4,5,6,8,0 are in the prime number, then it might be a circular prime. Convert the string to a queue for easier 
+            % rotation. 
+            Prime_Queue = queue:from_list( Prime_String ),  
+            % NOTE: Final argument in this function is the number of rotations
+            %       required. 
+            is_circular_prime( Primes, Prime_Queue, length( Prime_String ) )
+    end
 .
 is_circular_prime( _Primes, _Prime_Queue, 0 )
 ->
     true
 ;
-is_circular_prime( Primes, Prime_Queue, N )
+is_circular_prime( Primes, Prime_Queue, Rotations_Remaining )
 ->
     Rotated_Queue = rotate_queue( Prime_Queue ),
     Rotated_String = queue:to_list( Rotated_Queue ),
@@ -106,13 +136,29 @@ is_circular_prime( Primes, Prime_Queue, N )
         ;
         true
         ->
-            is_circular_prime( Primes, Rotated_Queue, N-1 )
+            is_circular_prime( Primes, Rotated_Queue, Rotations_Remaining-1 )
     end
 .
 
 
+prime_string_has_disallowed_digit( Prime_String, [Disallowed_Digit | Rest] )
+->
+    case lists:member([Disallowed_Digit], Prime_String ) of
+        true  ->  true;
+        false ->  prime_string_has_disallowed_digit( Prime_String, Rest )
+    end 
+;
+prime_string_has_disallowed_digit( _Prime_String, [] )
+->
+    false
+.
+
+
+% -----------------------------------------------------------------------
+% Queue Rotation. 
+% Takes whatever is in the front of the queue and inserts it at the back
+% -----------------------------------------------------------------------
 rotate_queue( Queue )
 ->
     queue:in( queue:get(Queue), queue:drop(Queue) )
 .
-
