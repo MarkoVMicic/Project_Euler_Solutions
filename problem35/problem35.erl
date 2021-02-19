6a98ceadf4e3d37beefc6d795630ed2345650448
MarkoVMicic <marko.v.micic@gmail.com>
2021-02-19 12:34:32 +0800

    Speed up erlang solution of problem 35 by improving prime sieve.
        The Sieve of Eratosthenes has been improved by filtering only
        up to √N, after which the remaining unfiltered numbers are
        all prime numbers. This results in 2 orders of magnitude
        speedup.

--- problem35/problem35.erl
+++ problem35/problem35.erl
@@ -24,36 +24,55 @@ main( N )
 % ----------------------
 % Sieve of Eratosthenes
 % ----------------------
-% TODO: Find a way to speed this up by only checking up to √N, after which all 
-%       remaining unfiltered numbers should be prime. 
-build_list_of_primes( N ) 
+build_list_of_primes( N )
 ->
-    build_list_of_primes( [2], lists:seq(2, N), [] )
+    build_list_of_primes( lists:seq(2, N), 2, [], [], math:sqrt(N) )
 .
-build_list_of_primes( Primes, [], [] ) 
--> 
-    lists:reverse( Primes )
-; 
-build_list_of_primes( Primes, [], Numbers )
+build_list_of_primes( [Current_Number | Unfiltered], 
+                      Current_Prime, 
+                      Filtered, 
+                      Primes,
+                      Root_N ) when Current_Number rem Current_Prime == 0
 ->
-    [Next_Prime | Rest] = lists:reverse( Numbers ),
-    build_list_of_primes( [Next_Prime | Primes], Rest, [] )
+    build_list_of_primes( Unfiltered, 
+                          Current_Prime,
+                          Filtered,
+                          Primes,
+                          Root_N )
 ;
-build_list_of_primes( [Current_Prime  | Primes], 
-                      [Current_Number | Numbers], 
-                      Filtered ) when Current_Number rem Current_Prime == 0
+build_list_of_primes( [Current_Number | Unfiltered], 
+                      Current_Prime, 
+                      Filtered,
+                      Primes, 
+                      Root_N )
 ->
-    build_list_of_primes( [Current_Prime | Primes],
-                          Numbers,
-                          Filtered )
+    build_list_of_primes( Unfiltered, 
+                          Current_Prime,
+                          [Current_Number | Filtered],
+                          Primes,
+                          Root_N )
 ;
-build_list_of_primes( [Current_Prime  | Primes], 
-                      [Current_Number | Numbers], 
-                      Filtered )
+build_list_of_primes( [], 
+                      Current_Prime,
+                      Filtered,
+                      Primes,
+                      Root_N ) when Current_Prime > Root_N
 ->
-    build_list_of_primes( [Current_Prime  | Primes],
-                          Numbers,
-                          [Current_Number | Filtered] )
+    lists:reverse(Primes) ++ lists:reverse( Filtered )
+;
+build_list_of_primes( [], 
+                      Current_Prime,
+                      Filtered,
+                      Primes,
+                      Root_N ) 
+->
+    New_Unfiltered = lists:reverse( Filtered ),
+    [New_Prime | _] = New_Unfiltered,
+    build_list_of_primes( New_Unfiltered,
+                          New_Prime,
+                          [],
+                          [Current_Prime | Primes],
+                          Root_N )
 .
 
 
