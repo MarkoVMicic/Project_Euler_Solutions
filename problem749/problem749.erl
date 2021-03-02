66132e3b4f26e8ec961b313e8c1245e55ce9f5d4
MarkoVMicic <marko.v.micic@gmail.com>
2021-03-02 13:26:34 +0800

    "Solve" problem 749 of project euler. Too slow!
        This program works with the test cases, but is just too slow
        to solve the given question. Some improvements are necessary
        to create a working solution. There are some obvious
        time-saves to be made e.g. checking which K to begin at by
        looking at N, and computing the sum of its digits, and
        raising that to K until the Kth power of the sum of digits is
        greater than N itself. This will eliminate a huge amount of
        function calls for larger N values.

--- problem749/problem749.erl
+++ problem749/problem749.erl
@@ -13,7 +13,8 @@
 % Find S(16).
 
 -module( problem749 ).
--export( [ main/1 ] ).
+-export( [ main/1,
+           is_near_power_sum/1 ] ).
 
 % Note: Number_Digits needs to be >= 2
 main( Number_Digits )
@@ -24,14 +25,27 @@ main( Number_Digits )
 
 s( Number_Digits )
 ->
-    % Start at 10, because no single digit number is a near power sum numbers
+    % Start at 10, because no single digit number is a near power sum number
     Initial_Value = 10,
-    Max_Value = exp(10, Number_Digits )
+    Max_Value = exp(10, Number_Digits ),
     s( Initial_Value, Max_Value, 0 )
 .
 s( Current_Value, Max_Value, Acc ) when Current_Value >= Max_Value
 ->
     Acc
+;
+s( Current_Value, Max_Value, Acc ) when Current_Value rem 100000 == 0
+->
+    io:fwrite("~p/~p~n", [Current_Value, Max_Value]),
+    case is_near_power_sum( Current_Value ) of
+        true
+        ->
+            s( Current_Value + 1, Max_Value, Acc + Current_Value )
+        ;
+        false
+        ->
+            s( Current_Value + 1, Max_Value, Acc )
+    end 
 ;
 s( Current_Value, Max_Value, Acc )
 ->
@@ -49,7 +63,53 @@ s( Current_Value, Max_Value, Acc )
 
 is_near_power_sum( Number )
 ->
-    false
+    is_near_power_sum( Number, 1, 0 )
+.
+is_near_power_sum( Number, K, Previous )
+->
+    Power_Sum = power_sum( Number, K ),
+    case Power_Sum >  Number + 1 orelse
+         Power_Sum =< Previous          of
+        true
+        ->
+            % If we have exceeded N+1 with the power sum, then we don't have a 
+            % near power sum number. 
+            % If the current Power_Sum is less than or equals to the previous 
+            % Power_Sum (from a smaller K), then we have a case where our 
+            % digits are all 0s or 1s only and therefore the Power_Sum will be 
+            % the same for all K, so we need to short-circuit here to prevent 
+            % infinite recursion. 
+            false
+        ;
+        false
+        ->
+            case Power_Sum == Number - 1 orelse 
+                 Power_Sum == Number + 1         of
+                true
+                ->
+                    true
+                ;
+                false
+                ->
+                    % Try the next positive integer K.
+                    is_near_power_sum( Number, K+1, Power_Sum )
+            end
+    end 
+.
+
+
+power_sum( Number, K )
+->
+    power_sum( integer_to_list(Number), K, 0 )
+.
+power_sum( [Current_Digit | Rest], K, Acc )
+->
+    Digit_Power = exp( list_to_integer([Current_Digit]), K ),
+    power_sum( Rest, K, Acc+Digit_Power )
+;
+power_sum( [], _K, Acc )
+->
+    Acc
 .
 
 
